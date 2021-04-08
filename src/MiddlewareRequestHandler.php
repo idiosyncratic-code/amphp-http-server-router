ba35c629017cb59359d949f4b18d1f3598562989
Jason Silkey <jason@jasonsilkey.com>
2021-04-07 22:37:31 -0700

    Start and stop default handler observer

--- src/MiddlewareRequestHandler.php
+++ src/MiddlewareRequestHandler.php
@@ -4,35 +4,22 @@ declare(strict_types=1);
 
 namespace Idiosyncratic\AmpRoute;
 
-use Amp\Http\Server\HttpServer;
 use Amp\Http\Server\Middleware;
 use Amp\Http\Server\Request;
 use Amp\Http\Server\RequestHandler;
 use Amp\Http\Server\ServerObserver;
-use Amp\Failure;
 use Amp\Promise;
-use Error;
-use Psr\Log\LoggerInterface;
-use SplObjectStorage;
 
 use function array_shift;
-use function array_walk;
-use function get_class;
-use function sprintf;
 
 final class MiddlewareRequestHandler implements RequestHandler, ServerObserver
 {
+    use HasServerObservers;
+
     private RequestHandler $requestHandler;
 
     /** @var array<Middleware> */
     private array $middleware;
-
-    /** @var SplObjectStorage<ServerObserver, null> */
-    private SplObjectStorage $observers;
-
-    private HttpServer $server;
-
-    private bool $running = false;
 
     public function __construct(
         RequestHandler $requestHandler,
@@ -41,8 +28,6 @@ final class MiddlewareRequestHandler implements RequestHandler, ServerObserver
         $this->requestHandler = $requestHandler;
 
         $this->middleware = $middleware;
-
-        $this->observers = new SplObjectStorage();
     }
 
     public function handleRequest(Request $request) : Promise
@@ -51,73 +36,9 @@ final class MiddlewareRequestHandler implements RequestHandler, ServerObserver
 
         $middleware = $handler->getNextHandler();
 
-        $currentHandler = $middleware instanceof Middleware ?
-            $middleware :
-            $this->requestHandler;
-
-        $this->getLogger()->debug(sprintf('Executing handler %s', get_class($currentHandler)));
-
         return $middleware instanceof Middleware ?
             $middleware->handleRequest($request, $handler) :
             $this->requestHandler->handleRequest($request);
-    }
-
-    /**
-     * @return Promise<mixed>
-     */
-    public function onStart(HttpServer $server) : Promise
-    {
-        if ($this->running) {
-            return new Failure(new Error('Router already started'));
-        }
-
-        $this->server = $server;
-
-        $this->running = true;
-
-        if ($this->requestHandler instanceof ServerObserver) {
-            $this->observers->attach($this->requestHandler);
-        }
-
-        array_walk(
-            $this->middleware,
-            function ($middleware) : void {
-                if (! ($middleware instanceof ServerObserver)) {
-                    return;
-                }
-
-                $this->observers->attach($middleware);
-            },
-        );
-
-        $promises = [];
-
-        foreach ($this->observers as $observer) {
-            $promises[] = $observer->onStart($server);
-        }
-
-        /** @phpstan-ignore-next-line */
-        return Promise\all($promises);
-    }
-
-    /**
-     * @return Promise<mixed>
-     */
-    public function onStop(HttpServer $server) : Promise
-    {
-        $this->running = false;
-
-        $promises = [];
-
-        foreach ($this->observers as $observer) {
-            $promises[] = $observer->onStop($server);
-        }
-
-        /** @phpstan-ignore-next-line */
-        $this->observers->removeAll($this->observers);
-
-        /** @phpstan-ignore-next-line */
-        return Promise\all($promises);
     }
 
     /**
@@ -129,8 +50,27 @@ final class MiddlewareRequestHandler implements RequestHandler, ServerObserver
         return array_shift($this->middleware);
     }
 
-    private function getLogger() : LoggerInterface
+    /**
+     * @return array<ServerObserver>
+     *
+     * phpcs:disable SlevomatCodingStandard.Classes.UnusedPrivateElements.UnusedMethod
+     */
+    private function getServerObservers() : array
     {
-        return $this->server->getLogger();
+        $observers = [];
+
+        if ($this->requestHandler instanceof ServerObserver) {
+            $observers[] = $this->requestHandler;
+        }
+
+        foreach ($this->middleware as $middleware) {
+            if (! ($middleware instanceof ServerObserver)) {
+                continue;
+            }
+
+            $observers[] = $middleware;
+        }
+
+        return $observers;
     }
 }
